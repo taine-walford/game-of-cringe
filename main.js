8ffdc3a9066fbb5dbab66977c597b0c1570c967e
taiaha <tainewalford@live.com>
2023-07-25 02:49:21 +1200

    'small changes'

--- main.js
+++ main.js
@@ -1,50 +1,45 @@
-let bg = null
-let bgctx = null
-
-let fg = null
-let fgctx = null
-
-let bgImage
-let bgData
-
-let fgImage
-let fgData
+let bg, bgctx, bgImage, bgData
+let fg, fgctx, fgImage, fgData
 
 const resX = 320
 const resY = 180
 
-const colorBG = [60, 25, 65, 255]
-const colorBGL = [45, 5, 50, 255]
-const colorFG = [175, 235, 255, 255]
-const colorFGX = [75, 135, 155, 255]
+const colorBG = [50, 20, 50, 255]
+const colorBGL = [10, 0, 10, 255]
+const colorFG = [150, 230, 255, 255]
+const colorFGX = [55, 50, 65, 255]
 const transparent = [0, 0, 0, 0]
 
-blank = new Array(320 * 180)
-activePoints = blank.map(x => {state: 'dead'})
+let playing = false
+activePoints = []
 
 window.onload = () => {
     bg = document.querySelector('canvas#bg')
     bgctx = bg.getContext('2d')
     fg = document.querySelector('canvas#fg')
     fgctx = fg.getContext('2d')
+    ren = document.querySelector('canvas#render')
+    rctx = ren.getContext('2d')
 
-    bgctx.mozImageSmoothingEnabled = false;
-    bgctx.webkitImageSmoothingEnabled = false;
-    fgctx.mozImageSmoothingEnabled = false;
-    fgctx.webkitImageSmoothingEnabled = false;
+    rctx.imageSmoothingEnabled = false
 
     bgImage = bgctx.createImageData(resX, resY);
     fgImage = fgctx.createImageData(resX, resY);
-    drawBG()
-    bgctx.putImageData(bgImage, 0, 0);
-    
-    fg.onclick = clickHandler
-    // // gameLoop()
+    rImage = rctx.createImageData(resX*4, resY*4);
 
-    // console.log(fgImage.data);
+    drawBG()
+
+    ren.onclick = clickHandler
 
     stepButton = document.querySelector('button#step')
     stepButton.onclick = stepHandler
+
+    playButton = document.querySelector('button#simulate')
+    playButton.onclick = playhandler
+
+    render()
+    let ticks = window.location.search.slice(1)
+    setInterval(simulate, 1000/ticks)
 }
 
 const drawBG = () => {
@@ -56,6 +51,9 @@ const drawBG = () => {
                 drawPixel(x, y, colorBG, bgImage.data)
         }
     }
+    bgctx.putImageData(bgImage, 0, 0);
+    rctx.scale(4,4)
+    rctx.drawImage(bg, 0, 0)
 }
 
 function drawPixel (x, y, color, data) {
@@ -69,117 +67,98 @@ function drawPixel (x, y, color, data) {
 }
 
 function updateAllPoints() {
-    // get neighbours of live cells
-    getNeighbours()
-    // store x, y, state, liveNeighs
-
-    // loop activePoints
-    //     handle dead
-    //     kill lives
-
-    // draw
+    tickPoints = []
+    activePoints = activePoints.filter(c => c.state == 'live')
+    activePoints = activePoints.filter(c => c.x > 0 && c.y > 0)
+    console.log(activePoints)
+    let nearPoints = getNear(activePoints)
+    tickPoints = [...activePoints, ...nearPoints]
+    activePoints = simulateStep(tickPoints)
 }
 
-function getNeighbours () {
-    for (let obj of activePoints) {
-        if(!obj) continue
-        for(let offX = obj.x - 8; offX < obj.x + 16; offX += 8) {
-            for(let offY = obj.y - 8; offY < obj.y + 16; offY += 8) {
-                if(!(offX == obj.x && offY == obj.y) && activePoints[offX + offY * resX].state != 'live') {
-                    activePoints[layerX + (resX * offY)] = {
-                        x: offX,
-                        y: offY,
-                        index: offX + (resX * offY),
-                        state: 'dead',
-                    }
-                }
+function getNear (array) {
+    let result = []
+    for (let obj of array) {
+        for (let offX = obj.x - 2; offX < obj.x + 4; offX += 2) {
+            for (let offY = obj.y - 2; offY < obj.y + 4; offY += 2) {
+                if ((offX == obj.x && offY == obj.y)) continue 
+                if (array.filter(p => p.x == offX && p.y == offY).length > 0) continue
+                if (result.filter(p => p.x == offX && p.y == offY).length > 0) continue
+                result.push({
+                    x: offX,
+                    y: offY,
+                    state: 'dead'
+                })
             }
         }
     }
+    return result
 }
 
-function simulateStep (points) {
-    for (let key in points) {
-        let coords = key.split(`|`)
-        currentX = Number(coords[0])
-        currentY = Number(coords[1])
-        currentNeighbours = checkNeighbours(currentX, currentY)
-        console.log('checking', `${currentX}|${currentY}`)
-        if(points[key] == "dead") {
-            if(currentNeighbours.live == 3) points[key] == 'live'
-        }
+function simulateStep (array) {
+    let result = []
+    for (let key in array) {
+        let c = array[key]
+        let near = array.filter(p => {
+            if (p.state != 'live') return false
+            if (p.x == c.x && p.y == c.y) return false
+            if (p.x < (c.x - 2) || p.x > (c.x + 2)) return false
+            if (p.y < (c.y - 2) || p.y > (c.y + 2)) return false
+            return true
+        }).length
+        if (c.state == 'dead' && near == 3) 
+            result.push({x: c.x, y: c.y, state: 'live'})
         else {
-            if(currentNeighbours.live != 2 && currentNeighbours.live != 3)
-                points[key] == 'dead'
+            if (near == 2 || near == 3)
+                result.push(c)
+            else 
+                result.push({x: c.x, y: c.y, state: 'dead'})
         }
     }
+    return result
 }
 
-function removeDeadPoints () {
-    for (let key in activePoints) {
-        if(activePoints[key] == 'dead') delete activePoints[key]
-    }
-}
-
-// function getNeighbourPoints () {
-//     console.log('initial check')
-//     for (let key in activePoints) {
-//         let coords = key.split(`|`)
-//         let x = Number(coords[0])
-//         let y = Number(coords[1])
-//         let neighbours = checkNeighbours(x, y)
-//         activePoints = {...activePoints, ...neighbours.newPoints}
-//     }
-// }
-
-// function checkNeighbours(x, y) {
-//     console.log('cell check  ', x, '|', y)
-//     let result = {
-//         newPoints: [],
-//         live: 0
-//     }
-//     for(let offX = x - 8; offX < x + 16; offX += 8) {
-//         for(let offY = y - 8; offY < y + 16; offY += 8) {
-//             console.log(`     ${offX}|${offY}`)
-//             if(!(offX == x && offY == y)) {
-//                 result.newPoints[`${offX}|${offY}`] = "dead"
-//             }
-//             if(Object.keys(activePoints).includes(`${offX}|${offY}`))
-//                 if(activePoints[`${offX}|${offY}`] == 'live') {
-//                     console.log('live')
-//                     result.live += 1
-//             }
-//         }
-//     }
-//     console.log(result)
-//     console.log()
-//     return result
-// }
-
-function drawActivePoints(points) {
-    for (let point in points) {
-        drawBlock(point.x, point.y, point.state == 'live' ? colorFG : transparent, fgImage.data)
+function drawPoints(array) {
+    for (let key in array) {
+        obj = array[key]
+        if (obj.state == 'live') drawPixel(obj.x, obj.y, colorFG, fgImage.data)
+        else drawPixel(obj.x, obj.y, colorBG, fgImage.data)
     }
     fgctx.putImageData(fgImage, 0, 0);
+    rctx.drawImage(fg, 0, 0)
 }
 
 
 const clickHandler = ({layerX, layerY}) => {
-    activePoints[layerX + (resX * layerY)] = {
-        x: layerX,
-        y: layerY,
-        index: layerX + (resX * layerY),
+    layerX -= 2
+    layerY -= 2
+    scaledX = (Math.floor(layerX / 8) * 2) + 1
+    scaledY = (Math.floor(layerY / 8) * 2) + 1
+    activePoints[scaledX + (resX * scaledY)] = {
+        x: scaledX,
+        y: scaledY,
         state: 'live',
     }
-    console.log(activePoints)
 }
 
 function stepHandler (evt) {
+    playing = true
     updateAllPoints()
+    playing = false
+}
+
+function playhandler() {
+    playing = playing ? false : true
+    playButton.textContent = playing ? 'PAUSE' : 'PLAY'
 }
 
 let simulate = () => {
-    requestAnimationFrame(simulate)
+    console.log('tick')
+    if(playing)
+        updateAllPoints()
+}
 
-    drawActivePoints()
+let render = () => {
+    drawPoints(activePoints)
+    requestAnimationFrame(render)
 }
