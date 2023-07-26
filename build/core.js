4331b8bcf7143c98b545d3bad799c0e51af0f828
taiaha <tainewalford@live.com>
2023-07-26 12:34:34 +1200

    remove build

--- build/core.js
+++
@@ -1,7 +0,0 @@
-console.log('core loaded');
-export function createCanvas(x, y) {
-    var canvas = document.createElement("canvas");
-    canvas.width = x;
-    canvas.height = y;
-    return canvas;
-}
